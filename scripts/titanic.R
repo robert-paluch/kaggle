835616161f5c424cd3c6b05fb8c9a4041b9a3e31
Robert Paluch <robert.paluch@pw.edu.pl>
2022-09-02 14:14:03 +0200

    Read data from csv files

--- scripts/titanic.R
+++ scripts/titanic.R
@@ -3,3 +3,12 @@
 #         READ DATA             #
 #                               #    
 #################################
+
+titanic.train <- read.csv("data/titanic/train.csv")
+titanic.test <- read.csv("data/titanic/test.csv")
+
+#################################
+#                               #
+#       COMPUTATIONS            #
+#                               #    
+#################################
